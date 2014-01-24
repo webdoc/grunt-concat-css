c5e889ba0f75da132cdc07601a6fbb08d5691afc
XhmikosR <xhmikosr@users.sourceforge.net>
2014-01-24 12:25:54 +0200

    Disable `rebaseUrls` by default.
    Refs Issue #2.

--- tasks/concat_css.js
+++ tasks/concat_css.js
@@ -19,7 +19,7 @@ module.exports = function(grunt) {
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
       base: "",
-      rebaseUrls: true,
+      rebaseUrls: false,
       debugMode: false
     });
 
